45b37bcd67141307bacd6c13e0b7ce80993f9714
Graham Watson <graham@phantomwatson.com>
2017-11-03 12:05:42 -0400

    Move HTML wrapper to layout.php

---
+++ src/View/layout.php
@@ -0,0 +1,16 @@
+<?php $title = '🤖 CBER Deploy-bot 🤖'; ?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>
+        <?= $title ?>
+    </title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+    <h1>
+        <?= $title ?>
+    </h1>
+    <?= $content ?>
+</body>
+</html>
--- webroot/index.php
+++ webroot/index.php
@@ -1,31 +1,14 @@
 <?php
-    $title = '🤖 CBER Deploy-bot 🤖';
     spl_autoload_register(function ($className) {
         include dirname(dirname(__FILE__)) . '/src/' . $className . '.php';
     });
     $deploy = new Deploy();
-?>
-<?php if (empty($_POST)): ?>
-    <!DOCTYPE HTML>
-    <html lang="en-US">
-    <head>
-        <meta charset="UTF-8">
-        <title>
-            <?= $title ?>
-        </title>
-    </head>
-    <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-        <h1>
-            <?= $title ?>
-        </h1>
-        <strong>
-            <?= $deploy->triggerMsg ?>
-        </strong>
-        <pre>
-            <?= $deploy->screenOutput->content ?>
-        </pre>
-    </body>
-    </html>
-<?php else: ?>
-    <?= $deploy->triggerMsg . "\n" . $deploy->screenOutput->content ?>
-<?php endif; ?>
+
+    if (empty($_POST)) {
+        $content =
+            '<strong>' . $deploy->triggerMsg . '</strong>' .
+            '<pre>' . $deploy->screenOutput->content . '</pre>';
+        include dirname(dirname(__FILE__)) . '/src/View/layout.php';
+    } else {
+        echo $deploy->triggerMsg . "\n" . $deploy->screenOutput->content;
+    }
