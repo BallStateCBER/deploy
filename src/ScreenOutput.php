2628e2221877ee131a098fc50b98c08178c306be
Graham Watson <graham@phantomwatson.com>
2017-11-10 13:01:43 -0500

    Put all app classes under App namespace

--- src/ScreenOutput.php
+++ src/ScreenOutput.php
@@ -1,4 +1,6 @@
 <?php
+namespace App;
+
 class ScreenOutput
 {
     public $content;
--- webroot/index.php
+++ webroot/index.php
@@ -1,15 +1,16 @@
 <?php
     $appDir = dirname(dirname(__FILE__));
     spl_autoload_register(function ($className) use ($appDir) {
+        $className = str_replace('App\\', '', $className);
         include  $appDir . '/src/' . $className . '.php';
     });
 
     if (empty($_POST) && empty($_GET)) {
-        $sites = Site::getSites();
+        $sites = App\Site::getSites();
         $content = include $appDir . '/src/View/home.php.template';
         include $appDir . '/src/View/layout.php.template';
     } else {
-        $deploy = new Deploy();
+        $deploy = new App\Deploy();
         if (empty($_POST)) {
             $content =
                 '<strong>' . $deploy->triggerMsg . '</strong>' .
