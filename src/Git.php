7449cc858244b5cf89178b9704c4fab40cda2f4b
Graham Watson <graham@phantomwatson.com>
2018-03-02 12:31:25 -0500

    Disable manual deployment links if repo is unpullable

---
+++ src/Git.php
@@ -0,0 +1,42 @@
+<?php
+namespace App;
+
+class Git
+{
+    /**
+     * @param $siteName
+     * @param $branch
+     * @throws \Exception
+     * @return bool
+     */
+    public static function canPull($siteName, $branch)
+    {
+        if (!Site::isValid($siteName)) {
+            throw new \Exception('Unrecognized site name: ' . $siteName);
+        }
+
+        $site = Site::getSite($siteName);
+        if (!Site::isValidBranch($branch, $site)) {
+            throw new \Exception("$branch branch does not exist for $siteName repo");
+        }
+
+        // Make sure site directory exists
+        $appDir = dirname(dirname(__FILE__));
+        $sitesRoot = dirname($appDir);
+        $siteDir = $sitesRoot . '/' . $site[$branch];
+        if (!file_exists($siteDir)) {
+            return false;
+        }
+
+        $sitesRoot = dirname($appDir);
+        $siteDir = $sitesRoot . '/' . $site[$branch];
+
+        // Change working directory to appropriate website
+        chdir($siteDir);
+
+        // Check to see if status mentions modified files
+        $results = shell_exec('git status');
+
+        return stripos($results, 'modified: ');
+    }
+}
